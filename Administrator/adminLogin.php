9bee1cc66abf177cfa91507f8e1faae33781589c
dian <sh.alqudree@gmail.com>
2022-05-03 04:16:28 +0800

    init

--- Administrator/adminLogin.php
+++ Administrator/adminLogin.php
@@ -20,7 +20,6 @@
         }
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,15 +27,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form Administrator</title>
-
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Theme/adminLogin.css">
+    <script src="https://kit.fontawesome.com/bcdb11579f.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <form action="#" method="post">
-        <input type="text" name="username" placeholder="Enter Admin ID"><br><br>
-        <input type="password" name="password" placeholder="Enter Password"><br><br>
-        
-        <button type="submit">Login</button>
-    </form>
+    <div class="logincard">
+        <div class="container">
+            <div>
+                <img src="Theme/admin.gif" alt="illustrations">
+            </div>
+
+            <div>
+                <form action="#" method="post">
+                <h1>FOODY ADMINISTRATOR</h1>
+                    <div class="inputBox">
+                        <input type="text" name="username" placeholder="Enter Admin ID">
+                        <div class="icon"><i class="fa-solid fa-user-large"></i></i></div>
+                    </div>
+
+                    <div class="inputBox">
+                        <input type="password" name="password" placeholder="Enter Password">
+                        <div class="icon"><i class="fa-solid fa-user-lock"></i></div>
+                    </div>
+                    
+                    <button type="submit" class="loginbutton">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
