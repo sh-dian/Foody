500b0e344fa21c5726835dfb02caa55f4c8da389
dian <sh.alqudree@gmail.com>
2022-05-16 02:59:48 +0800

    Navigation Bar

--- Administrator/HomePage.php
+++ Administrator/HomePage.php
@@ -7,34 +7,13 @@
     <title>Admin Home</title>
 
     <link rel="stylesheet" href="Theme/adminHome.css">
-    
-    <script src="JavaScript/function.js"></script>
 
+    <script src="JavaScript/function.js"></script>
     <script src="https://kit.fontawesome.com/bcdb11579f.js" crossorigin="anonymous"></script>
     
 </head>
-<body>  
-
-    <nav class="vNavbar">
-      <ul class="navbar-menu">
-        <li class="navbar-item">
-          <a href="#" class="navbar-link">
-            <i class="fa-solid fa-circle-user"></i><span>Home</span>
-          </a>
-        </li>
-
-      </ul>
-    </nav>
+<body> 
 
 
-
-<!--
-  <div id="mySidenav" class="sidenav">  
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        
-  </div>  
-
-  <span onclick="openNav()"><i class="fa-solid fa-bars"></i></span>
--->
 </body>
 </html>
