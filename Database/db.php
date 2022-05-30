e55258a36506156684bd9f01dd3588606706aba7
dian <sh.alqudree@gmail.com>
2022-05-30 09:41:39 +0800

    db connection

--- Database/db.php
+++ Database/db.php
@@ -2,7 +2,7 @@
     $sname = "localhost";
     $uname = "root";
     $password = "";
-    $db_name ="test";
+    $db_name ="foody";
 
     $con = mysqli_connect($sname, $uname, $password, $db_name);
    
