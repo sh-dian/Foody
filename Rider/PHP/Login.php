953745ba3dff64e98553dec2906e4b786516e0d3
LAPTOP-V0VQ33SS\anbuc <anbuchelvan65@gmail.com>
2022-06-03 01:12:30 +0800

    GG

--- Rider/PHP/Login.php
+++ Rider/PHP/Login.php
@@ -41,7 +41,7 @@
 
             <form action='#' method="post">
                 <div>
-                    <img src="logo.png" alt="Foody Logo" class="image">
+                    <img src="" alt="Foody Logo" class="image">
                     <h1>RIDER</h1>
                 </div>
 
