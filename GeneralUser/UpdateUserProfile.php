88448c49c86361223dd93eb7a6aa1b0da0ce6bd0
Amad <ahilman223@gmail.com>
2022-06-03 04:41:51 +0800

    CustProfile

---
+++ GeneralUser/UpdateUserProfile.php
@@ -0,0 +1,89 @@
+<?php
+    include_once 'C:\xampp\htdocs\Foody\Database\db.php';
+    session_start();
+
+    if(!isset($_SESSION["CustID"])){
+        header("Location: Login.php");
+    }
+    else{
+        if(isset($_POST["Update"])){
+
+            $adminName = mysqli_real_escape_string($con, $_POST["Cust_Name"]);
+            $adminEmail = mysqli_real_escape_string($con, $_POST["Cust_Email"]);
+            $adminPass = mysqli_real_escape_string($con, $_POST["Cust_Password"]);
+
+            $query = "UPDATE Admin SET Cust_Name='$Cust_Name' , Cust_Email ='$Cust_Email', Cust_Password='$Cust_Password' WHERE Cust_ID = '{$_SESSION["CustID"]}'";
+            $result = mysqli_query($con, $query);
+            
+            if($result){
+                echo "
+                <script>
+                    alert('Updated Success!');
+                    window.location = 'UserProfile.php';
+                </script>";
+
+            }else{
+                echo "<script>alert('Updated FAILED');</script>";
+                echo $con->error;
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Customer Profile</title>
+
+    <link rel="stylesheet" href="CSS/updateCustProfile.css">
+</head>
+<body>
+
+    <!-- Navigation Bar -->
+    <?php include "./NavigationBar.php" ?>
+
+    <h1>Update Customer Profile</h1>
+
+    <form action="" method="post">
+        <?php
+            $query = "SELECT * FROM Customer WHERE Cust_ID = '{$_SESSION["Cust_ID"]}' ";
+            $result = mysqli_query($con, $query);
+
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+        ?>
+
+            <div class="updateCustomer">
+                <div class="inputBox">
+                    <span class="details">Customer ID :</span>
+                    <input type="tezt" id="Cust_ID" name="Cust_ID" value="<?php echo $row['Cust_ID'] ?>" disabled required><br><br>
+                </div>
+                
+                <div class="inputBox">
+                    <span class="details">Customer Name :</span>
+                    <input type="text" id="Cust_Name" name="Cust_Name" value="<?php echo $row['Cust_Name'] ?>"><br><br>
+                </div>
+                
+                <div class="inputBox">
+                    <span class="details">Customer Email :</span>
+                    <input type="email" id="Cust_Email" name="Cust_Email" value="<?php echo $row['Cust_Email'] ?>"><br><br>
+                </div>
+
+                <div class="inputBox">
+                    <span class="details">Customer Password :</span>
+                    <input type="text" id="Cust_Password" name="Cust_Password" value="<?php echo $row['Cust_Password'] ?>"><br><br>
+                </div>
+            </div>
+        <?php
+                }
+            }
+        ?>
+
+            <button type="submit" class="btn" name="Update">Update Profile</button>
+    </form>
+
+</body>
+</html>
---
+++ GeneralUser/UserProfile.php
@@ -0,0 +1,68 @@
+<?php
+    include_once 'C:\xampp\htdocs\Foody\Database\db.php';
+    session_start();
+
+    if(!isset($_SESSION["Cust_login"])){
+        header("Location: Login.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Profile</title>
+
+    <link rel="stylesheet" href="CSS/userProfile.css"/>
+
+</head>
+<body>
+    <!-- Navigation Bar -->
+    <?php include "./NavigationBar.php" ?>
+
+    <h1>Customer Profile</h1>
+
+    <form action="" method="post">
+        <?php
+            $query = "SELECT * FROM Customer WHERE Cust_PhoneNum = '{$_SESSION["Cust_login"]}' ";
+            $result = mysqli_query($con, $query);
+
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+        ?>
+
+        <div class="adminProfile">
+
+                <div class="inputBox">
+                    <span class="details">Customer ID :</span>
+                    <input type="tezt" id="Cust_ID" name="Cust_ID" value="<?php echo $row['Cust_ID"'] ?>" disabled required><br><br>
+                </div>
+                
+                <div class="inputBox">
+                    <span class="details">Customer Name :</span>
+                    <input type="text" id="Cust_Name" name="Cust_Name" value="<?php echo $row['Cust_Name'] ?>" disabled required><br><br>
+                </div>
+                
+                <div class="inputBox">
+                    <span class="details">Customer Email :</span>
+                    <input type="email" id="Cust_Email" name="Cust_Email" value="<?php echo $row['Cust_Email'] ?>" disabled required><br><br>
+                </div>
+
+                <div class="inputBox">
+                    <span class="details">Password :</span>
+                    <input type="password" id="Cust_Password" name="Cust_Password" value="<?php echo $row['Cust_Password'] ?>" disabled required><br><br>
+                </div>
+        </div>
+
+        <?php
+                }
+            }
+        ?>    
+    </form>
+
+    <a href="UpdateUserProfile.php"><button type="submit" name="send" value="Edit" class="button">Edit</button></a>
+
+</body>
+</html>
