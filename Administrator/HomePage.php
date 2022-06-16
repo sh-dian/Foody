d9b3cd1d077a15305503765aaf1d34031471c110
dian <sh.alqudree@gmail.com>
2022-06-17 01:53:21 +0800

    init

--- Administrator/HomePage.php
+++ Administrator/HomePage.php
@@ -90,9 +90,73 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.0/dist/chart.min.js"></script>
     <script src="https://kit.fontawesome.com/bcdb11579f.js" crossorigin="anonymous"></script>
 
-    <div class="graphBox">
-        <div class="box"><canvas id="myChart"></canvas></div>
+    <?php 
+        $query = $con->query("
+            SELECT 
+            MONTH(Order_DeliveryTime) as month,
+                SUM(Order_Total) as amount
+            FROM orderrecord
+            GROUP BY month
+        ");
+
+        foreach($query as $data)
+        {
+            $month[] = $data['month'];
+            $amount[] = $data['amount'];
+        }
+
+    ?>
+
+
+    <div style="width: 80%; margin-left:8%;">
+        <canvas class="card" id="myChart"></canvas>
     </div>
+ 
+    <script>
+        // === include 'setup' then 'config' above ===
+        const labels = <?php echo json_encode($month) ?>;
+        const data = {
+            labels: labels,
+            datasets: [{
+            label: 'Monthly Profit',
+            data: <?php echo json_encode($amount) ?>,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+            options: {
+                responsive:true,
+            }
+            },
+        };
+
+        var myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    </script>
 
 </body>
 </html>
